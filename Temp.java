bb62203bf80ddae4547d4711fc24f7e18d4f2da6
smperczynski <smperczynski@gmail.com>
2016-10-12 15:43:41 +0200

    Changed engine

---
+++ Temp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Temp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		sc.nextLine();
+		String word = sc.nextLine();
+
+		System.out.println(number);
+		System.out.println(word);
+
+		sc.close();
+	        }
+	    }
